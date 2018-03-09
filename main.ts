07bbcb711efd8c635c9b80c925ffa2fb85a0399f
L-eeX <1035868977@qq.com>
2018-03-09 11:48:45 +0800

    2018-3-9

--- main.ts
+++ main.ts
@@ -5,6 +5,11 @@
 namespace CMPS11 {
     const CMPS11_ADDRESS = 0x60
 
+    let pitch = 0
+    let roll = 0
+    let bearing = 0
+    let fine = 0
+    let softVer = 0
 
     function i2cWriteByte(value: number) {
         pins.i2cWriteNumber(CMPS11_ADDRESS, value, NumberFormat.UInt8BE)
@@ -14,6 +19,10 @@ namespace CMPS11 {
         return pins.i2cReadBuffer(CMPS11_ADDRESS, num);
     }
 
+    function getSoftVer() {
+        i2cWriteByte(0)
+        return i2cRead(1)[0]
+    }
 
     /**
      * Execute a 42BYGH1861A-C step motor(Degree).
@@ -25,12 +34,55 @@ namespace CMPS11 {
         i2cWriteByte(2)
         let data = pins.createBuffer(4)
         data = i2cRead(4)
-        let highByte=data[0]
-        let lowByte=data[1]
-        let pitch = data[2]
-        let roll = data[3]
-        let bearing = ((highByte<<8)+lowByte)
-        let fine = ((highByte<<8)+lowByte)%10
-        //let softVer = getSoftVer()
+        let highByte = data[0]
+        let lowByte = data[1]
+        pitch = data[2]
+        roll = data[3]
+        bearing = ((highByte<<8)+lowByte)
+        fine = ((highByte<<8)+lowByte)%10
+        softVer = getSoftVer()
+    }
+
+    /**
+     * Execute a 42BYGH1861A-C step motor(Degree).
+     * M1_M2/M3_M4.
+    */
+    //% weight=70
+    //% blockId=CMPS11_softVer block="Software version"
+    export function softVer(): void {
+        return softVer
+    }
+
+    /**
+     * Execute a 42BYGH1861A-C step motor(Degree).
+     * M1_M2/M3_M4.
+    */
+    //% weight=80
+    //% blockId=CMPS11_pitch block="Pitch"
+    export function pitch(): void {
+        return pitch
+    }
+
+    /**
+     * Execute a 42BYGH1861A-C step motor(Degree).
+     * M1_M2/M3_M4.
+    */
+    //% weight=80
+    //% blockId=CMPS11_roll block="Roll"
+    export function roll(): void {
+        return roll
+    }
+
+    /**
+     * Execute a 42BYGH1861A-C step motor(Degree).
+     * M1_M2/M3_M4.
+    */
+    //% weight=80
+    //% blockId=CMPS11_bearing block="Bearing"
+    export function bearing(): void {
+        let bear = pins.createBuffer(2)
+        bear[0] = bearing
+        bear[1] = fine
+        return bear
     }
 }
