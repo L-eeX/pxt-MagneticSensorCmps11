23bd3207a830e86c18f6699f08d9e20ec705140e
L-eeX <1035868977@qq.com>
2018-03-09 11:53:27 +0800

    2018-3-9

--- main.ts
+++ main.ts
@@ -79,7 +79,7 @@ namespace CMPS11 {
     */
     //% weight=80
     //% blockId=CMPS11_bearing block="Bearing"
-    export function bearing(): PacketBuffer {
+    export function bearing(): number {
         let bear = pins.createBuffer(2)
         bear[0] = bearing
         bear[1] = fine
