5cd15c1d0dcf00004bf5c0cd706ee16c59818185
L-eeX <1035868977@qq.com>
2018-03-09 10:25:14 +0800

    2018-3-9

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
 /**
  *This is DFRobot:motor user motor and steering control function.
  */
-//% weight=10 color=#DF6721 icon="\uf013" block="DF-Driver"
+//% weight=10 color=#DF6721 icon="\uf013" block="CMPS11"
 namespace CMPS11 {
     const CMPS11_ADDRESS = 0x60
-    //% block="42"
-    Ste1 = 1
-    //% block="28"
-    Ste2 = 2
+
 
     function i2cWriteByte(value: number) {
         pins.i2cWriteNumber(CMPS11_ADDRESS, value, NumberFormat.UInt8BE)
@@ -17,18 +14,4 @@ namespace CMPS11 {
         return pins.i2cReadBuffer(CMPS11_ADDRESS, num);
     }
 
-
-
-    /**
-     * Steering gear control function.
-     * S1~S8.
-     * 0°~180°.
-    */
-    //% blockId=motor_servo block="Servo|%index|degree|%degree"
-    //% weight=100
-    //% degree.min=0 degree.max=180
-    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=4
-    export function servo(index: number, degree: number): void {
-
-    }
 }
